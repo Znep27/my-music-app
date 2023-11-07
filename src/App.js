c9eac7a782d3dea8a066c0f7f27eb9dbe4d1f8fb
Marc Rozenburgh <znep27@hotmail.com>
2023-11-07 17:14:45 -0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,52 +1,56 @@
-
-import './App.css';
-import * as React from 'react';
-import { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import { useState } from "react";
+import "./App.css";
+import AppBarComponent from "./Components/AppBarComponent";
+import Dashboard from "./Components/DashBoard";
+import OnlineMode from "./Components/OnlineMode";
+import MasterVolume from "./Components/MasterVolume";
+import SoundQuality from "./Components/SoundQuality";
 
 function App() {
-    const loggedIn = useState(false);
-    return (
-      <div>
-        <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar variant="dense">
-              <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" color="inherit" component="div">
-                My Music App
-              </Typography>
-            </Toolbar>
-          </AppBar>
-        </Box>
-        <Box
-          component="form"
-          sx={{
-            '& > :not(style)': { m: 1, width: '25ch'},
-          }}
-          noValidate
-          autoComplete="off"
-          className="login"
-        >
-          <TextField id="username" label="Username*" variant="standard" /><br />
-          <TextField id="password" label="Password*" variant="standard" /><br />
-        
-        </Box>
-        <Stack spacing={2} direction="row" className="login">
-          <Button variant="contained" className="login">LOGIN</Button>
-        </Stack>
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isOnline, setIsOnline] = useState(true);
+  const [volume, setVolume] = useState(30);
+  const [soundQuality, setSoundQuality] = useState(2);
+  return (
+    <div className="App">
+      <AppBarComponent />
+
+      {isLoggedIn ? (
+        <div className="display-area">
+          <h1 sx={{ display: "block" }}>Welcome User!</h1>
+          <div className="dashboard">
+            <OnlineMode className="box-component" isOnline={isOnline} setIsOnline={setIsOnline} />
+            <MasterVolume className="box-component" volume={volume} setVolume={setVolume} />
+            <SoundQuality className="box-component" soundQuality={soundQuality} setSoundQuality={setSoundQuality} />
+          </div>
+        </div>
+      ) : (
+        <Dashboard setLoggedIn={setLoggedIn} />
+      )}
+      
+      <div className="notification-section">
+        {isLoggedIn && <h2>System Notifications</h2>}
+        {isLoggedIn && !isOnline && (
+          <p>
+            Your application is offline. You won't be able to share or stream
+            music to other devices.
+          </p>
+        )}
+        {isLoggedIn && volume >= 80 && (
+          <p>
+            Listening to music at a high volume could cause long-term hearing
+            loss.
+          </p>
+        )}
+        {soundQuality < 2 && (
+          <p>
+            Music quality is degraded. Increase quality if your connection
+            allows it.
+          </p>
+        )}
       </div>
-    );
+    </div>
+  );
 }
 
 export default App;
-
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,88 +1,47 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Switch from '@mui/material/Switch';
-import Slider from '@mui/material/Slider';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-  
-  const label = { inputProps: { 'aria-label': 'Switch demo' } };
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
-  function valuetext(value) {
-    return `${value}°C`;
-  }
-
-  const card1 = (
-    <React.Fragment>
-      <CardContent>
-        <Typography variant="h5" component="div">
-          Online Mode
-        </Typography>
-        <Typography variant="body2">
-          Is this application connected to the internet?
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Switch {...label} />
-      </CardActions>
-    </React.Fragment>
+function Dashboard(props) {
+  return (
+    <Box
+      component="form"
+      sx={{
+        display: "grid",
+        width: "300px",
+        height: "300px",
+        gridGap: "10px",
+        backgroundColor: "white",
+        margin: "10px auto",
+      }}
+      noValidate
+      autoComplete="o"
+    >
+      <TextField
+        id="email-field"
+        label="Email"
+        variant="outlined"
+        type="email"
+        required
+      ></TextField>
+      <TextField
+        id="password-field"
+        label="Password"
+        variant="outlined"
+        type="password"
+        required
+      ></TextField>
+      <Button
+        variant="contained"
+        onClick={() => {
+          props.setLoggedIn(true);
+        }}
+      >
+        Login
+      </Button>
+    </Box>
   );
+}
 
-  const card2 = (
-    <React.Fragment>
-      <CardContent>
-        <Typography variant="h5" component="div">
-          Master Volume
-        </Typography>
-        <Typography variant="body2">
-          Overrides all other sound settings in this application
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Box sx={{ width: 300 }}>
-            <Slider
-                aria-label="Temperature"
-                defaultValue={30}
-                getAriaValueText={valuetext}
-                valueLabelDisplay="auto"
-                step={10}
-                marks
-                min={10}
-                max={110}
-            />
-        </Box>
-      </CardActions>
-    </React.Fragment>
-  );
-
-  const card3 = (
-    <React.Fragment>
-      <CardContent>
-        <Typography variant="h5" component="div">
-          Sound Quality
-        </Typography>
-        <Typography variant="body2">
-          Manually control the music quality in event of poor connection
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Switch {...label} />
-      </CardActions>
-    </React.Fragment>
-  );
-
-  function Dashboard() {
-    return (
-      <Box sx={{ minWidth: 275 }}>
-        <Card variant="outlined">{card1}</Card>
-      </Box>
-    );
-  }
-
-  export default Dashboard;
+export default Dashboard;
